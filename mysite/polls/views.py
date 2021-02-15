44c26ecdde4e5cbb83f4a47079e9e90dff6ac2b5
Alan Tseng <atseng202@gmail.com>
2021-02-15 14:07:09 -0800

    FInish pt 3 view functions/templating/namespacing urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world, At poll index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    # shortcut to load template, give context, render html
+    return render(request, 'polls/index.html', context)
 
+# Leave the rest of the views (detail, results, vote) unchanged
+
+
+def detail(request, question_id):
+    # shortcut like Flask get_or_404
+    # question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
